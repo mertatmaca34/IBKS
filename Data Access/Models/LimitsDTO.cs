268b5b51555c87978d7b9f407fcb930e0a0624d8
Mert Atmaca <mertatmaca34@gmail.com>
2023-02-21 16:24:23 +0300

    ++

---
+++ Data Access/Models/LimitsDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access.Models
+{
+    public class LimitsDTO
+    {
+        public int AkmLimit { get; set; }
+        public int KoiLimit { get; set; }
+        public int PhLimit { get; set; }
+    }
+}
