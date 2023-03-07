2c717c7b7d112dddaa4434def9ab232db0dd577d
Mert Atmaca <mertatmaca34@gmail.com>
2023-03-07 17:13:20 +0300

    ++

---
+++ Calibration/Models/CalibrationDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calibration.Models
+{
+    public class CalibrationDTO
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Parameter { get; set; }
+        public double ZeroRef { get; set; }
+        public double ZeroMeas { get; set; }
+        public double ZeroDiff { get; set; }
+        public double ZeroStd { get; set; }
+        public double SpanRef { get; set; }
+        public double SpanMeas { get; set; }
+        public double SpanDiff { get; set; }
+        public double SpanStd { get; set; }
+        public double ResultFactor { get; set; }
+    }
+}
